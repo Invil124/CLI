cae2a62cb07a3132a5eaf93e07c928ba94c5146f
Invil124 <113246425+Invil124@users.noreply.github.com>
2022-10-01 15:56:17 +0300

    Update bot.py
    change logick

--- bot.py
+++ bot.py
@@ -1,88 +1,89 @@
 
-def main():
-    CONNTACTS = {}
-    
-    def error_func(hanndler):
 
-        def wrapper():
-            try:
-                hanndler()
-            except IndexError as error_int:
-                print("Give me name and phone please")
-            except KeyError as er:
-                print("Wrong name try aggain")
+CONNTACTS = {}
 
-        return wrapper
+def error_func(handler):
+    def wrapper(*args, **kwargs):
+        try:
+            return handler(*args, **kwargs)
+        except IndexError as error_int:
+            print("Give me name and phone please")
+        except KeyError as er:
+            print("Wrong name try aggain")
+    return wrapper
 
+def greet_func(*args):
+    return print("How can I help you?")
 
-    def greet_func():
-        return print("How can I help you?")
-
-    def show_func():
-        for name, phone in CONNTACTS.items():
-            name = name.title()
-            print(f"{name}: {phone}")
+def show_func(*args):
+    for name, phone in CONNTACTS.items():
+        name = name.title()
+        print(f"{name}: {phone}")
         
-    @error_func
-    def change_func():
-        name = input("Enter current name: ")
-       
-        name = name.strip().lower()
-        if not name in CONNTACTS.keys():
-            return print("Current name not find.")
-        new_phone = input("Enter new phone number: ")
-        if not (new_phone.isdigit() and (13 >= len(new_phone) >=9)):
-            return print("Invalid phone number.Try again.")    
-        CONNTACTS[name] = new_phone
-        print("Phone successfully changed")
+@error_func
+def change_func(*args):
+    contact = args[0] #лист мінімум з двома аргументами
+    if len(contact) < 2:
+        return print("Inpunt current name and new phone:")
+    name = contact[0]
+    new_phone = contact[1] 
+    new_phone = new_phone.replace("+","") 
+    name = name.lower()
+    if name not  in CONNTACTS:
+        return print("Current name not find.")
+    if not (new_phone.isdigit() and (13 >= len(new_phone) >=9)):
+        return print("Invalid phone number.Try again.")    
+    CONNTACTS[name] = new_phone
+    print("Phone successfully changed")
 
+@error_func
+def add_func(*args):
+    contact = args[0] #лист мінімум з двома аргументами
+    name = contact[0].lower()
+    phone = contact[1]
+    phone = phone.replace("+","") 
+    if not (phone.isdigit() and (13 >= len(phone) >=9)):
+        return print("Invalid phone number.Try again.")
+    CONNTACTS[name] = phone
+    print("Phone add sucesfully")
 
-    @error_func
-    def add_func():
-        contact = input("Enter name and phone : ")
-        contact = contact.strip().split()
-        name = contact[0].lower()
-        phone = contact[1]
-        if not (phone.isdigit() and (13 >= len(phone) >=9)):
-            return print("Invalid phone number.Try again.")
-        CONNTACTS[name] = phone
-        print("Phone add sucesfully")
-
-    @error_func
-    def phone_func():
-        contact = input("Enter name: ")
-        contact = contact.strip().lower()
-        print (CONNTACTS[contact])
+@error_func
+def phone_func(*args):
+    contact = args[0]
+    name = contact[0].lower()
+    print (CONNTACTS[name])
    
+def quit_func(*args):
+    print("Good bye!")
+    return quit()
 
-    def quit_func():
-        print("Good bye!")
-        return quit()
-
+@error_func
+def entered_command(command):
+    
+    command = command.strip().split(" ")
 
     COMANDS = {
         "exit":quit_func,
-        "good bye":quit_func,
+        "good":quit_func,
         "close":quit_func,
         "hello":greet_func,
-        "show all":show_func,
+        "show":show_func,
         "change" : change_func,
         "add" : add_func,
-        "phone":phone_func
-    }
+        "phone":phone_func}
+    
+    if command[0] not in COMANDS:
+        return print("Command not found")
+    COMANDS[command[0]](command[1:])
+
+
+
+def main():
 
     while True:
         command = input("Enter command: ")
-        command = command.strip()
+        entered_command(command)
         
-        if command not in COMANDS:
-            print ("Unknown command!")
-            continue
-
-        COMANDS[command]()
-
-        
-
 
 if __name__ == "__main__":
     exit(main())
